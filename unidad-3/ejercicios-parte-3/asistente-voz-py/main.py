447df2de945a5e7487f262aeb3023d75739fcf6f
juangc.zip <jgarciac111@educastillalamancha.es>
2024-11-12 10:35:57 +0100

    sdasdasd

--- unidad-3/ejercicios-parte-3/asistente-voz-py/main.py
+++ unidad-3/ejercicios-parte-3/asistente-voz-py/main.py
@@ -1,23 +1,111 @@
  # import libraries ->
 import typer
 import sys
+import os
+import platform
 import time
+import yfinance as yf
+import pyjokes
+import webbrowser
+import wikipedia
 from rich import print
-from virtualAssistant import *
+from virtualAssistant import __talk, __audioToText, __getCurrentDate, __getCurrentTime
 
+
+#———————————————————————————————————————————————————————————————————————————————————————————————————
  # main function ->
 def __main ():
+  __clearConsole()
   __scriptLoading()
   
+  __talk("Hola! Soy SEN tu asistente personal. ¿En que puedo ayudarte?")
 
+  while True:
+    query = "chiste" #__audioToText()
+    
+    if 'youtube' in query:
+      __talk('Abriendo YouTube en tu Navegador Predeterminado.')
+      webbrowser.open('https://www.youtube.com')
+      break
+    
+    elif 'google' in query:
+      __talk('Abriendo Google en tu Navegador Predeterminado.')
+      webbrowser.open('https://www.google.com')
+      break
+      
+    elif 'hora' in query:
+      currentTime = __getCurrentTime()
+      __talk(f'Son las {currentTime} actualmente.')
+      break
+      
+    elif 'fecha' in query:
+      currentDate = __getCurrentDate()
+      __talk(f"Estamos a {currentDate}")
+      break
+    
+    elif 'chiste' in query:
+      chiste = pyjokes.get_joke(language='es', category='neutral')
+      __talk(chiste)
+      break
+      
+    elif 'precio acciones' in query:
+      __talk('¿De que compañia te gustaria saber el precio de las acciones?')
+      company = __audioToText()
+      
+      
+      try:
+        accion = yf.Ticker(company)
+        price = accion.info['regularMarketPrice']
+        __talk(f"El precio actual de las acciones de la compañia {company} es de {price}")
+        break
+      except:
+        __talk('Lo siento pero no he podidio encontrar informacion sobre las compañias.')
+        break
+    
+    elif 'wikipedia' in query:
+      __talk('¿Sobre que quieres informacion?')
+      searchQuery = __audioToText()
+      
+      try:
+        result = wikipedia.summary(searchQuery, sentences=2)
+        __talk(f"Segun Wikipedia: {result}")
+        break
+      except:
+        __talk("Lo siento pero no he podidio encontrar informacion de ese tema.")
+        break
+        
+    elif 'salir' in query or 'adios' in query:
+      __talk("Adios. Que pases un buen dia.")
+      break
+    
+  print("[bold blue] > Operacion Realizada.[/]")
+  
+  print("[bold white]+ Opciones Disponibles: [/]")
+  print("[bold white] 1. Quiero Continuar.[/]")
+  print("[bold white] 0. Salir del Programa.[/]")
+  opcion = int(typer.prompt("  - Quieres continuar: (0/1)"))
+  if (opcion == 1):
+    __main()
+  else:
+    SystemExit()
+
+
+#———————————————————————————————————————————————————————————————————————————————————————————————————
  # rest functions ->
+  # function | clearConsole | limpia la terminal -->
+def __clearConsole():
+  os.system('cls' if platform.system() == 'Windows' else 'clear') 
+  
+ #————————————————————————————————————————————————————————————————————————————————————————
   # function | welcomeMessage | mensaje de bienvenida ->
 def __welcomeMessage ():
   print("[bold yellow] > Bienvenido a tu Asistente Virtual by rezzt.dev[/]")
 
+ #————————————————————————————————————————————————————————————————————————————————————————
+  # function | scriptLoading | carga del script ->
 def __scriptLoading ():
   chars = "|/-\\"
-  for _ in range(20):
+  for _ in range(5):
     for char in chars:
       sys.stdout.write(f"\r{char} [script loading...]")
       sys.stdout.flush()
@@ -26,6 +114,8 @@ def __scriptLoading ():
   print("\n[bold green]+ script loaded.[/]")
   __welcomeMessage
 
+
+#———————————————————————————————————————————————————————————————————————————————————————————————————
  # save runner ->
 if __name__ == "__main__":
   typer.run(__main)
--- unidad-3/ejercicios-parte-3/asistente-voz-py/virtualAssistant.py
+++ unidad-3/ejercicios-parte-3/asistente-voz-py/virtualAssistant.py
@@ -1,5 +1,82 @@
  # import libraries ->
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import requests
+import os
 
+from rich import print
+from dotenv import load_dotenv
 
+ # constants & variables ->
+OPENWEATHER_API_KEY = "f890390af0e545f23382edb7645a4547"
 
+#———————————————————————————————————————————————————————————————————————————————————————————————————
+ # load & initialize ->
+load_dotenv()
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+recognizer = sr.Recognizer()
+
+#———————————————————————————————————————————————————————————————————————————————————————————————————
  # rest functions ->
+  # function | audioToText | convertir una cadena de audio a texto ->
+def __audioToText ():
+  with sr.Microphone() as source:
+    print("[bold blue] > Listening...[/]")
+    
+    recognizer.adjust_for_ambient_noise(source)
+    audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
+    
+  try:
+    print("[bold blue] > Recognizing...[/]")
+    textAudio = recognizer.recognize_google(audio, language='es-ES')
+    return textAudio.lower()
+  except sr.UnknownValueError:
+    print("[bold red]/ERROR! Speech not Understood.[/]")
+    return ''
+  except sr.RequestError:
+    print("[bold red]/ERROR! Could not request results from speech recognition service.[/]")
+    return ''
+
+ #————————————————————————————————————————————————————————————————————————————————————————
+  # function | talk | pronunciar un mensaje utilizando texto a voz ->
+def __talk(message):
+  engine.say(message)
+  engine.runAndWait()
+  
+ #————————————————————————————————————————————————————————————————————————————————————————
+  # function | getCurrentTime | obtener la hora actual en formato legible ->
+def __getCurrentTime ():
+  now = datetime.datetime.now()
+  return now.strftime("%I:%M %p")
+
+ #————————————————————————————————————————————————————————————————————————————————————————
+  # function | getCurrentDate | obtener la fecha actual en formato legible ->
+def __getCurrentDate ():
+  now = datetime.datetime.now()
+  return now.strftime("%A, %d %B %Y")
+
+ #————————————————————————————————————————————————————————————————————————————————————————
+  # function | getWeather | obtiene informacion del clima de un ciudad concreta ->
+def __getWeather(inputCity):
+  apiKey = os.getenv(OPENWEATHER_API_KEY)
+  baseUrl = "http://api.openweathermap.org/data/2.5/weather"
+  params = {
+    'q': inputCity,
+    'appid': apiKey,
+    'units': 'metric'
+  }
+  
+  response = requests.get(baseUrl, params=params)
+  data = response.json()
+  
+  if data['cod'] == 200:
+    temp = data['main']['temp']
+    description = data['weather'][0]['description']
+    return f"[bold green] > The temperature in {inputCity} is {temp}°C with {description}.[/]"
+  else:
+    return f"/ERROR! Sorry, I couldn't fetch the weather information."
